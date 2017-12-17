44c82d238d75a71a68de854afb8015c3f84d4caa
Kjell Breiland <kjell.breiland@gmail.com>
2017-12-17 12:54:40 -0800

    us bash to run entrypoing and functionalize entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 
-# tmux set -g status off && tmux attach 2> /dev/null
-
-if [ "$RUST_UPDATE" == "1" ]
-  then
-    ./rustserver update-lgsm
-    ./rustserver update
+if [ "$1" != "" ]; then
+  if ["$1" == "start" ]; then
+    install
+    start
+  fi
+else
+  install
 fi
 
-if [ "$RUST_FORCE_UPDATE" == "1" ]
-  then
+# Install is actuall install or update
+function install() {
+  if [ "$RUST_UPDATE" == "1" ]; then
+      ./rustserver update-lgsm
+      ./rustserver update
+  fi
+  
+  if [ "$RUST_FORCE_UPDATE" == "1" ]; then
+      ./rustserver update-lgsm
+      ./rustserver force-update
+  fi
+
+  if [ !-d ./serverfiles ]; then
     ./rustserver update-lgsm
-    ./rustserver force-update
-fi
+    ./rustserver auto-install
+  fi
+}
 
-./rustserver start
 
-# trap exit signals
-trap stop INT SIGINT SIGTERM
+function start() {
+  if [ !-d ./serverfiles ]; then
+    install
+  else
+     # trap exit signals
+    trap stop INT SIGINT SIGTERM
+    ./rustserver start
+    sleep 10
+    running
+  fi
+}
 
 # stop the rust server
 function stop() {
@@ -27,11 +48,14 @@ function stop() {
   exit 0
 }
 
-# attach to the tmux session
-tmux set -g status off && tmux attach 2> /dev/null
+# running is for when running the docker to keep the image and server going
+function running() {
+  # attach to the tmux session
+  tmux set -g status off && tmux attach 2> /dev/null
 
-# if something fails while attaching to the tmux session then just wait
-while :
-do
-  sleep 360
-done
+  # if something fails while attaching to the tmux session then just wait
+  while : ; do
+    sleep 360
+  done
+}
+
